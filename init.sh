de8554a419a9af99b3590e52cd203312ee6928d4
Raphaël Proust <raphlalou@gmail.com>
2011-11-07 09:26:33 +0000

    [screenrc] use ^B (tmux like)
    Going back to screen? Tmux tneds to mess vim up!)

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ ln -s -i $PWD/vimrc         $HOME/.vimrc
 ln -s -i $PWD/gvimrc        $HOME/.gvimrc
 ln -s -i $PWD/pentadactylrc $HOME/.pentadactylrc
 ln -s -i $PWD/tmux.conf     $HOME/.tmux.conf
+ln -s -i $PWD/screenrc      $HOME/.screenrc
 ln -s -i $PWD/nethackrc     $HOME/.nethackrc
 ln -s -i $PWD/gitconfig     $HOME/.gitconfig
 
