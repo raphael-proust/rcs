97e0f8eb450ed4e8fb7ac49d6890f0bbd40a0699
Raphaël Proust <raphlalou@gmail.com>
2011-11-09 06:57:59 +0000

    [teledactylrc] first version
    [init.sh] add teledactylrc entry

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 ln -s -i $PWD/vimrc         $HOME/.vimrc
 ln -s -i $PWD/gvimrc        $HOME/.gvimrc
 ln -s -i $PWD/pentadactylrc $HOME/.pentadactylrc
+ln -s -i $PWD/teledactylrc  $HOME/.teledactylrc
 ln -s -i $PWD/tmux.conf     $HOME/.tmux.conf
 ln -s -i $PWD/screenrc      $HOME/.screenrc
 ln -s -i $PWD/nethackrc     $HOME/.nethackrc
