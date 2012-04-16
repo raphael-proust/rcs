11d3b81c0867d6ce86e3ccd64c928756ffebfa47
Raphaël Proust <raphlalou@gmail.com>
2012-04-16 13:31:17 +0200

    [togglevga.sh] detect when VGA1 is not connected

--- togglevga.sh
+++ togglevga.sh
@@ -3,12 +3,17 @@
 IN="LVDS1"
 EXT="VGA1"
 
-if (xrandr | grep "$EXT" | grep "+")
-    then
+if (xrandr | grep "$EXT" | grep "disconnected")
+then
+  xrandr --output $EXT --off --output $IN --auto
+else
+  if (xrandr | grep "$EXT" | grep "+")
+  then
     xrandr --output $EXT --off --output $IN --auto
-    else
-        if (xrandr | grep "$EXT" | grep " connected")
-            then
-            xrandr --output $IN --off --output $EXT --auto
-        fi
+  else
+    if (xrandr | grep "$EXT" | grep " connected")
+    then
+      xrandr --output $IN --off --output $EXT --auto
+    fi
+  fi
 fi
