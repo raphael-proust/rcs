ad266970b2e0571ef70d4f2cf82ff02226ea9ab7
Raphaël Proust <raphlalou@gmail.com>
2012-06-28 12:09:08 +0200

    [wmiirc,xinitrc] let set_bg manage bg
    also, apply to togglevga

--- home/user/bin/togglevga.sh
+++ home/user/bin/togglevga.sh
@@ -6,17 +6,14 @@ EXT="VGA1"
 if (xrandr | grep "$EXT" | grep "disconnected")
 then
 	xrandr --output $EXT --off --output $IN --auto
-	set_bg.sh
 else
 	if (xrandr | grep "$EXT" | grep "+")
 	then
 		xrandr --output $EXT --off --output $IN --auto
-		set_bg.sh
 	else
 		if (xrandr | grep "$EXT" | grep " connected")
 		then
 			xrandr --output $IN --off --output $EXT --auto
-			set_bg.sh
 		fi
 	fi
 fi
