395e3d529e3e7277d2ca395dd050982483b93b1d
Raphaël Proust <raphlalou@gmail.com>
2012-06-28 15:24:45 +0200

    [pkgs/dwm] MPlayer is Floating

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -17,8 +17,9 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{  NULL,      NULL,       NULL,       0,            False,      -1        },
+	/* class      instance    title       tags mask     isfloating  monitor */
+	{ "MPlayer",  NULL,       NULL,       0,            True,       -1       },
+	{  NULL,      NULL,       NULL,       0,            False,      -1       },
 };
 
 /* layout(s) */
