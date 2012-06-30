2b7b61a9540c3a4c59692eb2abe9a18a46cf9b9e
Raphaël Proust <raphlalou@gmail.com>
2012-06-30 09:28:50 +0200

    [bin] dash is da shell

--- home/user/bin/install-all.sh
+++ home/user/bin/install-all.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if $(ls /bin/dash)
+then
+  #okay
+else
+  echo "WARNING: /bin/dash not found. Installed scripts won't work."
+
 for i in $(ls *-bnwr*)
 do
   ln -f -s $PWD/$i $HOME/bin/
