3d9e12c4787008881d6e821f36b1a404b7b0725e
Raphaël Proust <raphlalou@gmail.com>
2012-07-01 19:49:04 +0200

    [pkgs/dwm] MPlayer is Floating
    And no NULL entry

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -19,7 +19,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating  monitor */
 	{ "MPlayer",  NULL,       NULL,       0,            True,       -1       },
-	{  NULL,      NULL,       NULL,       0,            False,      -1       },
 };
 
 /* layout(s) */
