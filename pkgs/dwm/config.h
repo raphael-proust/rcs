a9d12fefd3f107b9e34d8250df51f690cc394d12
Raphaël Proust <raphlalou@gmail.com>
2012-07-05 16:08:17 +0200

    switch to urxvt
    Affects many configuration variables, makes it necessary to have a .Xresources.

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
---
+++ pkgs/st/config.h
@@ -0,0 +1,73 @@
+#define TAB 8
+#define TNAME "st-256color"
+#define XFT_FONT "monospace-8"
+#define BORDER 2
+#define SHELL "/bin/sh"
+
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white"
+};
+
+/* Default colors (colorname index)
+   foreground, background, cursor   */
+#define DefaultFG 7
+#define DefaultBG 0
+#define DefaultCS 1
+
+/* Special keys (change & recompile st.info accordingly)
+   Keep in mind that kpress() in st.c hardcodes some keys.
+
+   Mask value:
+   * Use XK_ANY_MOD to match the key no matter modifiers state
+   * Use XK_NO_MOD to match the key alone (no modifiers)
+   
+      key,        mask,  output */
+static Key key[] = {
+	{ XK_BackSpace, XK_NO_MOD, "\177" },
+   	{ XK_Insert,    XK_NO_MOD, "\033[2~" },
+	{ XK_Delete,    XK_NO_MOD, "\033[3~" },
+	{ XK_Home,      XK_NO_MOD, "\033[1~" },
+	{ XK_End,       XK_NO_MOD, "\033[4~" },
+	{ XK_Prior,     XK_NO_MOD, "\033[5~" },
+	{ XK_Next,      XK_NO_MOD, "\033[6~" },
+	{ XK_F1,        XK_NO_MOD, "\033OP"   },
+	{ XK_F2,        XK_NO_MOD, "\033OQ"   },
+	{ XK_F3,        XK_NO_MOD, "\033OR"   },
+	{ XK_F4,        XK_NO_MOD, "\033OS"   },
+	{ XK_F5,        XK_NO_MOD, "\033[15~" },
+	{ XK_F6,        XK_NO_MOD, "\033[17~" },
+	{ XK_F7,        XK_NO_MOD, "\033[18~" },
+	{ XK_F8,        XK_NO_MOD, "\033[19~" },
+	{ XK_F9,        XK_NO_MOD, "\033[20~" },
+	{ XK_F10,       XK_NO_MOD, "\033[21~" },
+	{ XK_F11,       XK_NO_MOD, "\033[23~" },
+	{ XK_F12,       XK_NO_MOD, "\033[24~" },
+};
+
+/* Line drawing characters (sometime specific to each font...) */
+static char gfx[] = {
+	['f'] = 'o',
+	['g'] = '+',
+	['i'] = '#',
+	[255] = 0,
+};
+
+/* double-click timeout (in milliseconds) between clicks for selection */
+#define DOUBLECLICK_TIMEOUT 200
+#define TRIPLECLICK_TIMEOUT (2*DOUBLECLICK_TIMEOUT)
