928706cdd9cfdd7391eff9d70571530f776bee0f
Raphaël Proust <raphlalou@gmail.com>
2012-07-06 11:22:30 +0200

    [pkgs/dwm] no pkg-config

--- pkgs/dwm/config.mk
+++ pkgs/dwm/config.mk
@@ -8,16 +8,15 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/include/X11
 X11LIB = /usr/lib/X11
 
+XFTINC = /usr/include/freetype2
+
 # Xinerama
 XINERAMALIBS  = -L${X11LIB} -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC} -I/usr/include/freetype2
-INCS += `pkg-config --cflags xft`
-
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
-LIBS += `pkg-config --libs xft`
+INCS = -I. -I/usr/include -I${X11INC} -I${XFTINC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS} -lXft
 
 # flags
 CPPFLAGS += -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
