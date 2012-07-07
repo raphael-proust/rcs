250419fd210ecca4a590f769b56159841eb9840f
Raphaël Proust <raphlalou@gmail.com>
2012-07-07 16:25:22 +0200

    [pkgs/dwm] add bottom-stack patch
    Patch is customized a bit.

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -26,10 +26,12 @@ static const float mfact      = 0.55;  /* factor of master area size [0.05..0.95
 static const int nmaster      = 1;     /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
+#include "bstack.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[M]",      monocle }, /* first entry is default */
 	{ "[]=",      tile },
+	{ "TTT",      bstack },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
@@ -64,7 +66,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
