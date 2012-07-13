729fab339521deb34989eb799ef52738df9c9357
Raphaël Proust <raphlalou@gmail.com>
2012-07-13 20:37:08 +0200

    [bin/task] better semantics

--- home/user/bin/install-all.sh
+++ home/user/bin/install-all.sh
@@ -5,7 +5,7 @@ then
   echo "WARNING: /bin/dash not found. Installed scripts won't work."
 fi
 
-for i in $(ls *-bnwr*) gvim vpaste dvtm
+for i in $(ls *-bnwr*) task gvim vpaste dvtm
 do
   ln -f -s $PWD/$i $HOME/bin/
 done
