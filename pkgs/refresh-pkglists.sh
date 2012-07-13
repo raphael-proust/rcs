7c261f9093c9445c3440477384220cf54eae2443
Raphaël Proust <raphlalou@gmail.com>
2012-07-13 15:06:53 +0200

    [pkgs] pkglist.x and pkglist.nox
    Also, refresh-pkglists generates/refreshes these lists.

--- pkgs/refresh-pkglists.sh
+++ pkgs/refresh-pkglists.sh
@@ -2,8 +2,11 @@
 
 pacman -Qeq | sort > /tmp/pacman-Qeq-list
 pacman -Qmq | sort > /tmp/pacman-Qmq-list
+pacman -Rcs -p --print-format "%n" libx11 | sort > /tmp/pacman-Rcs-x11-list
 comm -23 /tmp/pacman-Qeq-list /tmp/pacman-Qmq-list > pkglist.full
 comm -12 /tmp/pacman-Qeq-list /tmp/pacman-Qmq-list > aur-pkglist
-cat pkglist.full | grep -v libreoffice | grep -v texlive > pkglist
-cat pkglist.full | grep libreoffice > pkglist.libreoffice
-cat pkglist.full | grep texlive > pkglist.texlive
+comm -12 pkglist.full /tmp/pacman-Rcs-x11-list > pkglist.x
+comm -23 pkglist.full /tmp/pacman-Rcs-x11-list > pkglist.nox
+cat pkglist.full | grep -v libreoffice | grep -v texlive | sort > pkglist
+cat pkglist.full | grep libreoffice | sort > pkglist.libreoffice
+cat pkglist.full | grep texlive | sort > pkglist.texlive
