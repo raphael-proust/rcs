b90c5cfcc8167b1ad7dca8f42a5f25c13dfb513b
Raphaël Proust <raphlalou@gmail.com>
2012-11-04 21:58:47 +0000

    [pkgs/st] update and better config.h

--- pkgs/st/config.h
+++ pkgs/st/config.h
@@ -1,11 +1,23 @@
-#define TAB 8
-#define TNAME "st-256color"
-#define FONT "monospace:antialias=true:autohint=true"
-#define BORDER 2
-#define SHELL "/bin/sh"
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static char font[] = "monospace:pixelsize=12:antialias=true:autohint=true";
+static int borderpx = 2;
+static char shell[] = "/bin/sh";
+
+/* double-click timeout (in milliseconds) between clicks for selection */
+static unsigned int doubleclicktimeout = 300;
+static unsigned int tripleclicktimeout = 600;
+
+/* TERM value */
+static char termname[] = "st-256color";
+
+static unsigned int tabspaces = 8;
+
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	/* 8 normal colors */
 	"black",
 	"red3",
 	"green3",
@@ -14,6 +26,8 @@ static const char *colorname[] = {
 	"magenta3",
 	"cyan3",
 	"gray90",
+
+	/* 8 bright colors */
 	"gray50",
 	"red",
 	"green",
@@ -22,29 +36,36 @@ static const char *colorname[] = {
 	"magenta",
 	"cyan",
 	"white",
+
 	[255] = 0,
+
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#f3f0f0",
-	"#000300",
+	"#f2f0f0",
+	"#000500",
 	"#aa4444",
 	"#333333",
 };
 
-/* Default colors (colorname index)
-   foreground, background, cursor, unfocused cursor */
-#define DefaultFG  256
-#define DefaultBG  257
-#define DefaultCS  258
-#define DefaultUCS 259
 
-/* Special keys (change & recompile st.info accordingly)
-   Keep in mind that kpress() in st.c hardcodes some keys.
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, unfocused cursor
+ */
+static unsigned int defaultfg = 256;
+static unsigned int defaultbg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultucs = 259;
 
-   Mask value:
-   * Use XK_ANY_MOD to match the key no matter modifiers state
-   * Use XK_NO_MOD to match the key alone (no modifiers)
+/*
+ * Special keys (change & recompile st.info accordingly)
+ * Keep in mind that kpress() in st.c hardcodes some keys.
+ *
+ * Mask value:
+ * * Use XK_ANY_MOD to match the key no matter modifiers state
+ * * Use XK_NO_MOD to match the key alone (no modifiers)
+ */
 
-      key,        mask,  output */
+/* key, mask, output */
 static Key key[] = {
 	{ XK_BackSpace, XK_NO_MOD, "\177" },
 	{ XK_Insert,    XK_NO_MOD, "\033[2~" },
@@ -76,6 +97,3 @@ static Shortcut shortcuts[] = {
 	{ MODKEY|ShiftMask,	XK_Next,	xzoom,		{.i = -1} },
 };
 
-/* double-click timeout (in milliseconds) between clicks for selection */
-#define DOUBLECLICK_TIMEOUT 200
-#define TRIPLECLICK_TIMEOUT (2*DOUBLECLICK_TIMEOUT)
