b6336a15b63a5b88dd91a2aed045661de9f5e828
Raphaël Proust <raphlalou@gmail.com>
2013-04-23 22:17:01 +0100

    pkgs/dwm: switching toggleview and view

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -45,10 +45,10 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      comboview,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      combotag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,                       KEY, toggleview, {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY, view,  {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY, toggletag,  {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY, tag,   {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
