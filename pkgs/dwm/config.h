0fe7a8a1a311151f09aad68ecd55854062f0659e
Raphaël Proust <raphlalou@gmail.com>
2013-04-23 22:25:15 +0100

    pkgs/dwm: swap view/toggleview and tag/toggletag for mouse

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -106,9 +106,9 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,            0,              Button1,        toggleview,     {0} },
+	{ ClkTagBar,            0,              Button3,        view,           {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        toggletag,      {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        tag,            {0} },
 };
 
