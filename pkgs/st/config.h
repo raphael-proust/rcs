cc39eb5bc41a679c65820c225c373ef9b9bdef3e
Raphaël Proust <raphlalou@gmail.com>
2013-06-27 08:15:05 +0100

    pkgs/st: increase default font size
    There's no bold mode for terminus-12

--- pkgs/st/config.h
+++ pkgs/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";
+static char font[] = "-*-terminus-medium-r-*-*-14-*-*-*-*-*-*-*";
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 
