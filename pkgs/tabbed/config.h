4e25fd28ba3199f13faf63ddd0c733b54566da50
Raphaël Proust <raphlalou@gmail.com>
2013-08-13 17:44:26 +0100

    tabbed: build from git head

--- pkgs/tabbed/config.h
+++ pkgs/tabbed/config.h
@@ -1,24 +1,36 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
-static const char normbgcolor[] = "#222222";
-static const char normfgcolor[] = "#bbbbbb";
-static const char selbgcolor[]  = "#005577";
-static const char selfgcolor[]  = "#eeeeee";
+static const char font[]        = "-*-terminus-medium-r-*-*-14-*-*-*-*-*-*-*";
+static const char normbgcolor[] = "#eee8d5";
+static const char normfgcolor[] = "#268bd2";
+static const char selbgcolor[]  = "#fdf6e3";
+static const char selfgcolor[]  = "#657b83";
 static const char before[]      = "<";
 static const char after[]       = ">";
 static const int  tabwidth      = 200;
 static const Bool foreground    = False;
+
+/*
+ * Where to place a new tab when it is opened. When npisrelative is True,
+ * then the current position is changed + newposition. If npisrelative
+ * is False, then newposition is an absolute position.
+ */
+static int  newposition   = 0;
+static Bool npisrelative  = False;
 
 #define MODKEY ControlMask
 static Key keys[] = { \
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_Return, focusonce,      { 0 } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          { 0 } },
+
 	{ MODKEY|ShiftMask,             XK_l,      rotate,         { .i = +1 } },
 	{ MODKEY|ShiftMask,             XK_h,      rotate,         { .i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movetab,        { .i = -1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movetab,        { .i = +1 } },
 	{ MODKEY,                       XK_Tab,    rotate,         { .i = 0 } },
+
 	{ MODKEY,                       XK_1,      move,           { .i = 0 } },
 	{ MODKEY,                       XK_2,      move,           { .i = 1 } },
 	{ MODKEY,                       XK_3,      move,           { .i = 2 } },
@@ -29,5 +41,9 @@ static Key keys[] = { \
 	{ MODKEY,                       XK_8,      move,           { .i = 7 } },
 	{ MODKEY,                       XK_9,      move,           { .i = 8 } },
 	{ MODKEY,                       XK_0,      move,           { .i = 9 } },
+
 	{ MODKEY,                       XK_q,      killclient,     { 0 } },
+
+	{ 0,                            XK_F11,    fullscreen,     { 0 } },
 };
+
