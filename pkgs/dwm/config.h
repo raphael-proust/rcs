7f1114d0c3fb323836bf525bb6a032116d99760e
Raphaël Proust <raphlalou@gmail.com>
2014-03-04 11:56:48 +0000

    pkgs/dwm: better borders, no bstack

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -35,14 +35,10 @@ static const float mfact      = 0.60;  /* factor of master area size [0.05..0.95
 static const int nmaster      = 1;     /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
-#include "bstack.c"
-#include "bstackhoriz.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
