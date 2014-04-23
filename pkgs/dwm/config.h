a8d9dbffd7978e57e8b146c19599ce81077b429e
Raphaël Proust <raphlalou@gmail.com>
2014-04-24 00:46:52 +0100

    pkgs/dwm: remove bindings for unused layouts

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -74,8 +74,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      toggleview,     {.ui = ~0 } },
