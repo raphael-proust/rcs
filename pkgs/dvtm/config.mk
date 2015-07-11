731fbcb363a46879bd94640ddc3f93cd497a7d6e
Raphaël Proust <raphlalou@gmail.com>
2015-07-11 18:59:06 +0100

    pkgs/dvtm: update and attach-aside patch

---
+++ pkgs/dvtm/config.mk
@@ -0,0 +1,19 @@
+# dvtm version
+VERSION = 0.14
+
+# Customize below to fit your system
+
+PREFIX ?= /usr/local
+MANPREFIX = ${PREFIX}/share/man
+TERMINFO := ${DESTDIR}${PREFIX}/share/terminfo
+
+INCS = -I.
+LIBS = -lc -lutil -lncursesw
+CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_XOPEN_SOURCE_EXTENDED
+CFLAGS += -std=c99 ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS}
+LDFLAGS += ${LIBS}
+
+DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall -Wextra -Wno-unused-parameter
+
+CC ?= cc
+STRIP ?= strip
