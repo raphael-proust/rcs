62d5b09458f28cb261249d843c3fbb494c8b63ed
Raphaël Proust <code@bnwr.net>
2018-10-08 16:22:57 +0800

    pkgs/some update

--- pkgs/quark/config.h
+++ pkgs/quark/config.h
@@ -1,51 +1,34 @@
-/* quark configuration */
+#define HEADER_MAX 4096
+#define FIELD_MAX  200
 
-static const char *servername = "127.0.0.1";
-static const char *serverport = "80";
-static const char *chrootdir  = ".";
-static const char *docroot    = ".";
-static const char *docindex   = "index.html";
-static const char *user       = "nobody";
-static const char *group      = "nobody";
-static const char *cgi_dir    = ".";
-static const char *cgi_script = "/werc.rc";
-static int cgi_mode           = 0;
-static int allowdirlist       = 0;
-
-static const MimeType servermimes[] = {
-	{ "html", "text/html; charset=UTF-8" },
-	{ "htm",  "text/html; charset=UTF-8" },
-	{ "css",  "text/css" },
-	{ "c",    "text/plain" },
-	{ "h",    "text/plain" },
-	{ "go",   "text/plain" },
-	{ "sh",   "text/plain" },
-	{ "log",  "text/plain" },
-	{ "txt",  "text/plain" },
-	{ "md",   "text/plain" },
-	{ "pdc",   "text/plain" },
-	{ "png",  "image/png" },
-	{ "gif",  "image/gif" },
-	{ "jpeg", "image/jpeg" },
-	{ "jpg",  "image/jpeg" },
-	{ "webp", "image/webp" },
-	{ "xml",  "application/xml" },
-	{ "js",   "application/javascript" },
-	{ "bz2",  "application/x-bzip2" },
-	{ "gz",   "application/x-gzip" },
-	{ "xz",   "application/x-xz" },
-	{ "tgz",  "application/x-gtar" },
-	{ "pdf",  "application/x-pdf" },
-	{ "tar",  "application/x-tar" },
-	{ "zip",  "application/zip" },
-	{ "iso",  "application/x-iso9660-image" },
-	{ "ogx",  "application/ogg" },
-	{ "flac", "audio/flac" },
-	{ "mp3",  "audio/mp3" },
-	{ "oga",  "audio/ogg" },
-	{ "ogg",  "audio/ogg" },
-	{ "avi",  "video/x-msvideo" },
-	{ "mp4",  "video/mp4" },
-	{ "ogv",  "video/ogg" },
-	{ "webm", "video/webm" }
+/* mime-types */
+static const struct {
+	char *ext;
+	char *type;
+} mimes[] = {
+	{ "xml",   "application/xml" },
+	{ "xhtml", "application/xhtml+xml" },
+	{ "html",  "text/html; charset=UTF-8" },
+	{ "htm",   "text/html; charset=UTF-8" },
+	{ "css",   "text/css" },
+	{ "txt",   "text/plain" },
+	{ "md",    "text/plain" },
+	{ "c",     "text/plain" },
+	{ "h",     "text/plain" },
+	{ "gz",    "application/x-gtar" },
+	{ "tar",   "application/tar" },
+	{ "pdf",   "application/x-pdf" },
+	{ "png",   "image/png" },
+	{ "gif",   "image/gif" },
+	{ "jpeg",  "image/jpg" },
+	{ "jpg",   "image/jpg" },
+	{ "iso",   "application/x-iso9660-image" },
+	{ "webp",  "image/webp" },
+	{ "svg",   "image/svg+xml" },
+	{ "flac",  "audio/flac" },
+	{ "mp3",   "audio/mpeg" },
+	{ "ogg",   "audio/ogg" },
+	{ "mp4",   "video/mp4" },
+	{ "ogv",   "video/ogg" },
+	{ "webm",  "video/webm" },
 };
